5650ec503bc415cdb5d990f51414fe652209c349
Abhishek@1234 <abhishekpes123@gmail.com>
2023-08-30 11:40:15 +0530

    first commit

---
+++ function.js
@@ -0,0 +1,8 @@
+function makeTeaorcoffee(powder,coffee,tea){
+    console.log(`add ${powder} powder`);
+    console.log(`add ${coffee} powder`);
+    console.log(`add ${tea} powder`);
+    console.log("water");
+    console.log("sugar");
+}
+return makeTeaorcoffee("coffee")
